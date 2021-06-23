50d52d452927e050bdd8c3b971b7567839e027a1
Yiteng Huang <hyy528924543@gmail.com>
2021-06-23 11:37:22 +1000

    finish~~~

--- js/index.js
+++ js/index.js
@@ -19,44 +19,58 @@
       5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const start = document.querySelector('#start');
-  start.addEventListener('click', function (e) {
-    document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none';
+window.addEventListener("DOMContentLoaded", () => {
+  const start = document.querySelector("#start");
+  start.addEventListener("click", function (e) {
+    document.querySelector("#quizBlock").style.display = "block";
+    start.style.display = "none";
   });
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
   const quizArray = [
     {
-      q: 'Which is the third planet from the sun?',
-      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
+      q: "Which is the third planet from the sun?",
+      o: ["Saturn", "Earth", "Pluto", "Mars"],
       a: 1, // array index 1 - so Earth is the correct answer here
     },
     {
-      q: 'Which is the largest ocean on Earth?',
-      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      q: "Which is the largest ocean on Earth?",
+      o: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
-      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+      q: "What is the capital of Australia",
+      o: ["Sydney", "Canberra", "Melbourne", "Perth"],
+      a: 1,
+    },
+    {
+      q: "How many planets are in our solar system",
+      o: ["Six", "Seven", "Eight", "Nine"],
+      a: 2,
+    },
+    {
+      q: "Where is the Great Pyramid of Giza",
+      o: ["United States", "Egypt", "Australia", "Canada"],
       a: 1,
     },
   ];
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
+    const quizWrap = document.querySelector("#quizWrap");
+    let quizDisplay = "";
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
-                    <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
+                    <li class="list-group-item" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
+
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
+
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
+
                     <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
+                    
                     </ul>
                     <div>&nbsp;</div>`;
       quizWrap.innerHTML = quizDisplay;
@@ -71,19 +85,61 @@ window.addEventListener('DOMContentLoaded', () => {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
         let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
+        liElement = document.querySelector("#" + li);
+        radioElement = document.querySelector("#" + r);
 
         if (quizItem.a == i) {
-          //change background color of li element here
+          liElement.style.backgroundColor = "#676767";
         }
 
-        if (radioElement.checked) {
-          // code for task 1 goes here
-        }
+        if (radioElement.checked && quizItem.a === i) score++;
       }
     });
+    return score;
   };
+
+  // submit button
+  const btnSubmit = document.querySelector("#btnSubmit");
+  const scoreSpan = document.querySelector("#score");
+  btnSubmit.addEventListener("click", () => {
+    submitTrigger();
+  });
+  function submitTrigger() {
+    calculateScore();
+    btnSubmit.style.display = "none";
+    scoreSpan.innerHTML = `final score: ${calculateScore()} / 5`;
+  }
+
+  // reset button
+  const btnReset = document.querySelector("#btnReset");
+  btnReset.addEventListener("click", () => location.reload());
+
+  // timer
+  start.addEventListener("click", () => {
+    const timeString = document.querySelector("#time");
+    const timeMin = parseInt(timeString.innerHTML.split(":")[0]);
+    const timeSec = parseInt(timeString.innerHTML.split(":")[1]);
+
+    let totalSec = timeMin * 60 + timeSec;
+    const interval = setInterval(function () {
+      let minLeft, secLeft;
+
+      totalSec--;
+
+      minLeft =
+        Math.floor(totalSec / 60) < 10
+          ? "0" + Math.floor(totalSec / 60)
+          : Math.floor(totalSec / 60);
+      secLeft = totalSec % 60 < 10 ? "0" + (totalSec % 60) : totalSec % 60;
+
+      timeString.innerHTML = `${minLeft}:${secLeft}`;
+
+      if (totalSec === 0) {
+        clearInterval(interval);
+        submitTrigger();
+      }
+    }, 1000);
+  });
 
   // call the displayQuiz function
   displayQuiz();
